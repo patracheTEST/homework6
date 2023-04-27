17d6f1a9a11b15a2825b8164a7341b51c3e15177
patrache <ssk5442@naver.com>
2023-04-27 22:37:33 +0900

    [feat]: delete operation added

--- linked_list.c
+++ linked_list.c
@@ -226,12 +226,52 @@ int insertLast(headNode* h, int key) {
 	return 0;
 }
 
+/**
+ * @brief 
+ * 
+ * 처음 노드를 지워주는 함수
+ * 
+ * 1. 연결리스트가 비어 있으면 지울게 없다고 출력해준다.
+ * 2. 처음 노드를 가리키는 n을 만들어주고
+ * 3. 처음 노드를 가리키는 h->first가 다음 노드를 가리키게 하고
+ * 4. 처음 노드를 할당 해제 해준다.
+ * 
+ * @param h 
+ * @return int 
+ */
 int deleteFirst(headNode* h) {
+	if (h->first == NULL) {
+		printf("nothing to delete.\n");
+		return 0;
+	}
+	listNode* n = h->first;
 
+	h->first = n->link;
+	free(n);
 
 	return 0;
 }
 
+/**
+ * @brief 
+ * 
+ * 연결리스트에서 값을 찾아서 지우는 함수이다.
+ * 
+ * 1. 연결리스트가 비어있는지 확인하고 비어있으면 아무 작업도 안하고 함수를 종료한다.
+ * 2. 연결리스트의 처음 노드를 가리키는 n을 선언 (현재 노드로 비교), 이전 노드를 가리킬 null 값을 가지는 trail 선언
+ * 3. 노드를 하나씩 넘겨 가면서 (하나씩 넘겼을 때 그 값이 Null 이면은 반복문 종료)
+ * 	노드의 값이 지울려는 (찾는, 입력한) 값인지 확인하고
+ * 		지울려는 값일 경우
+ * 			그 노드가 첫 노드면 두번째 노드를 첫 노드로 바꿔주고(헤드노드가 가리키게 하고) free()로 지워준다.
+ * 			그 노드가 첫 노드가 아닐 경우에는 이전 노드의 다음 노드가 현재 노드의 다음 노드를 가리키게 하고 free()로 지워준다.
+ * 		지울려는 값이 아닌 경우
+ * 			trail, 과 n의 값을 바꿔줌으로 다음 비교 연산 가능케 한다.
+ * 4. 연결리스트 내에 찾는 값이 없을 경우 찾는 값이 없다고 출력해준다.
+ * 
+ * @param h 
+ * @param key 
+ * @return int 
+ */
 int deleteNode(headNode* h, int key) {
 
 	if (h->first == NULL) {
@@ -261,7 +301,46 @@ int deleteNode(headNode* h, int key) {
 	return 0;
 }
 
+/**
+ * @brief 
+ * 
+ * 마지막 노드를 지우는 함수이다.
+ * 
+ * 1. 연결리스트가 비어 있을 경우 지울게 없다고 출력
+ * 2. 이전 노드 역할을 해줄 trail과 현재 노드를 가리켜줄 n을 만들어준다.
+ * 3. 연결리스트에 노드가 하나밖에 없을 경우
+ * 		처음 노드를 가리키는 값을 Null로 바꿔주고 노드를 free() 해준다.
+ * 4. 연결리스트의 마지막 노드를 가리키게 while문으로 이동한 후
+ * 이전 노드(마지막의 전 노드)의 다음 노드의 연결을 끊은 후
+ * 현재노드를 지워준다.
+ * 
+ * @param h 
+ * @return int 
+ */
 int deleteLast(headNode* h) {
+
+	if (h->first == NULL)
+	{
+		printf("nothing to delete.\n");
+		return 0;
+	}
+
+	listNode* n = h->first;
+	listNode* trail = NULL;
+
+	if(n->link == NULL) {
+		h->first = NULL;
+		free(n);
+		return 0;
+	}
+
+	while(n->link != NULL) {
+		trail = n;
+		n = n->link;
+	}
+
+	trail->link = NULL;
+	free(n);
 
 	return 0;
 }
